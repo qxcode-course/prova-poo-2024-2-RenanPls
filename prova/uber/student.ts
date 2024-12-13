4a9efabb0423a8061ec73edf1265ce388efaace1
RenanPls <caiorsilva02@alu.ufc.br>
2024-12-13 18:24:45 +0000

    notebook com testes

--- prova/uber/student.ts
+++ prova/uber/student.ts
@@ -3,20 +3,126 @@ function input(): string { let X: any = input; X.L = X.L || require("fs").readFi
 function write(text: any, endl="\n") { process.stdout.write("" + text + endl); }
 export {};
 
+class Pessoa{
+    private nome: string
+    private dinheiro: number
+
+    constructor(nome: string, dinheiro: number){
+        this.nome = nome
+        this.dinheiro = dinheiro
+    }
+
+    getNome():string {
+        return this.nome
+    }
+
+    getDinheiro(): number {
+        return this.dinheiro
+    }
+
+    movDinheiro(valor: number){
+        this.dinheiro += valor
+    }
+}
+
+class Moto {
+    private custo: number =0
+    private motorista: Pessoa | null = null
+    private passageiro: Pessoa | null = null
+
+    setMotorista(motorista: Pessoa): boolean{
+        if(this.motorista == null){
+            this.motorista = motorista
+            return true
+        }
+        return false
+    }
+
+    tirarMotorita(): void {
+        this.motorista = null
+    }
+
+    setPassageiro(passageiro: Pessoa): boolean{
+        if(this.motorista !== null && this.passageiro === null){
+            this.passageiro = passageiro
+            return true
+        }
+        return false
+    }
+
+    tirarPassageiro(): Pessoa | null {
+        let passageiroAtual = this.passageiro
+        this.passageiro = null
+        return passageiroAtual
+    }
+
+    dirigir(distancia: number){
+        if(this.motorista !== null && this.passageiro !== null){
+            this.custo += distancia
+        }
+    }
+
+    acabarCorrida(): string {
+        if(this.motorista == null || this.passageiro == null){
+            return `fail: Sem motorista ou passageiro`
+        }
+
+        let total = this.custo
+        if(this.passageiro.getDinheiro() >= total){
+            this.passageiro.movDinheiro(-total)
+            this.motorista.movDinheiro(total)
+        } else{
+            let pago = this.passageiro.getDinheiro()
+            this.passageiro.movDinheiro(-pago)
+            this.motorista.movDinheiro(pago)
+        }
+
+        let nomePassageiro = this.passageiro.getNome()
+        let dinheiroFinal = this.passageiro.getDinheiro()
+
+        this.custo = 0
+        this.tirarPassageiro
+
+        return `${nomePassageiro}:${dinheiroFinal} leave`
+    }
+
+    show(): string {
+        let motorista = this.motorista ? `${this.motorista.getNome()}:${this.motorista.getDinheiro()}` : "None"
+        let passageiro = this.passageiro  ? `${this.passageiro.getNome()}:${this.passageiro.getDinheiro()}` : "None"
+
+        return `Cost: ${this.custo}, Driver: ${motorista}, Passenger: ${passageiro}`
+    }
+
+}
+
 class Adapter {
+    moto: Moto = new Moto
+    
     setDriver(name: string, money: number): void {
+        let motorista = new Pessoa(name, money)
+        if(!this.moto.setMotorista(motorista)){
+            console.log('fail já existe um motorista')
+        }
+
     }
 
     setPassenger(name: string, money: number): void {
+        let passageiro = new Pessoa(name, money)
+        if(!this.moto.setPassageiro(passageiro)){
+            console.log('fail: já existe um passageiro')
+        }
     }
 
     drive(distance: number): void {
+       this.moto.dirigir(distance)
     }
 
-    leavePassenger(): void {
+    tirarPassageiro(): void {
+        console.log(this.moto.acabarCorrida())
     }
 
     show(): void {
+        console.log(this.moto.show())
     }
 }
 
@@ -32,7 +138,7 @@ function main(): void {
         else if (args[0] === "setDriver") { adapter.setDriver(args[1], +args[2]);    }
         else if (args[0] === "setPass"  ) { adapter.setPassenger(args[1], +args[2]); }
         else if (args[0] === "drive"    ) { adapter.drive(+args[1]);                 }
-        else if (args[0] === "leavePass") { adapter.leavePassenger();                }
+        else if (args[0] === "leavePass") { adapter.tirarPassageiro();                }
         else if (args[0] === "show"     ) { adapter.show();                          }
         else                              { console.log("fail: command not found");  }
     }
